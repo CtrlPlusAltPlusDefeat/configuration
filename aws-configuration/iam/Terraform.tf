947aaeec4214864410cb8606fedf1b322e3f21de
Matthew Sutton <matthewsutton97@outlook.com>
2023-04-23 17:19:27 +0100

    Need ec2 permissions for secuirty groups.

--- aws-configuration/iam/Terraform.tf
+++ aws-configuration/iam/Terraform.tf
@@ -121,3 +121,26 @@ EOF
 
   provider = aws.europe_london
 }
+
+resource "aws_iam_user_policy" "terraform_policy_ec2" {
+  name     = "terraform-ec2"
+  user     = aws_iam_user.terraform.name
+  policy   = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+          "ec2:*"
+        ],
+      "Effect": "Allow",
+      "Resource": [
+          "*"
+        ]
+    }
+  ]
+}
+EOF
+
+  provider = aws.europe_london
+}
