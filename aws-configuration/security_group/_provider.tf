bc708696d47e21dfae6c2632f0ebd75f55558776
Matthew Sutton <matthewsutton97@outlook.com>
2023-04-23 17:18:39 +0100

    Update _provider.tf

--- aws-configuration/security_group/_provider.tf
+++ aws-configuration/security_group/_provider.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      configuration_aliases = [ aws.europe_ireland ]
+      configuration_aliases = [ aws.europe_london ]
     }
   }
 }
