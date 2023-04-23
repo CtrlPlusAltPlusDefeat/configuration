e753785445d143b4cead0d618c4ab0894016384f
Matthew Sutton <matthewsutton97@outlook.com>
2023-04-23 16:04:47 +0100

    Update Site.tf

--- aws-configuration/cloudfront/Site.tf
+++ aws-configuration/cloudfront/Site.tf
@@ -8,16 +8,6 @@ resource "aws_cloudfront_origin_access_control" "site_control" {
   provider = aws.europe_london
 }
 
-resource "aws_cloudfront_origin_access_control" "site_control" {
-  name                              = data.aws_s3_bucket.site_s3.bucket_domain_name
-  description                       = data.aws_s3_bucket.site_s3.bucket_domain_name
-  origin_access_control_origin_type = "s3"
-  signing_behavior                  = "always"
-  signing_protocol                  = "sigv4"
-
-  provider = aws.europe_london
-}
-
 resource "aws_cloudfront_distribution" "site" {
 
   enabled         = true
