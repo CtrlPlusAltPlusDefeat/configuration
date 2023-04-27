239de4f81c8db687c90a70247efdbe639f4af955
Matthew Sutton <matthewsutton97@outlook.com>
2023-04-27 15:37:13 +0100

    Roles required for api gatewy

---
+++ aws-configuration/iam_role/ApiGatewayExecution.tf
@@ -0,0 +1,20 @@
+resource "aws_iam_role" "apigateway_execution" {
+  name = "APIGatewayExecution"
+  assume_role_policy = data.aws_iam_policy_document.api_gateway_execution_policy_document.json
+
+  provider = aws.europe_london
+}
+
+resource "aws_iam_role_policy_attachment" "apigateway_execution_logging_policy" {
+  role       = aws_iam_role.apigateway_execution.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonAPIGatewayPushToCloudWatchLogs"
+
+  provider = aws.europe_london
+}
+
+resource "aws_iam_role_policy_attachment" "apigateway_execution_lambda_policy" {
+  role       = aws_iam_role.apigateway_execution.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaRole"
+
+  provider = aws.europe_london
+}
--- aws-configuration/iam_role/_data.tf
+++ aws-configuration/iam_role/_data.tf
@@ -14,3 +14,20 @@ data "aws_iam_policy_document" "lambda_execution_policy_document" {
 
   provider = aws.europe_london
 }
+
+data "aws_iam_policy_document" "apigateway_execution_policy_document" {
+  statement {
+    actions = [
+      "sts:AssumeRole",
+    ]
+
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["apigateway.amazonaws.com"]
+    }
+  }
+
+  provider = aws.europe_london
+}
