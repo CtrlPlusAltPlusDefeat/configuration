1fe0f685b85644734656cd464c1439e68df1ede3
Matthew Sutton <matthewsutton97@outlook.com>
2023-04-28 14:19:26 +0100

    Update Lambda.tf

--- aws-configuration/security_group/Lambda.tf
+++ aws-configuration/security_group/Lambda.tf
@@ -9,6 +9,17 @@ resource "aws_security_group" "lambda" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  
+
+  ingress {
+    from_port = 443
+    to_port   = 443
+    protocol  = "tcp"
+
+    security_groups = [
+      aws_security_group.lambda.id
+    ]
+  }
 
   provider = aws.europe_london
 }
