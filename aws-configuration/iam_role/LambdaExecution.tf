7d1b39446f827711f796b2d5efb543c89370c4cd
Matthew Sutton <matthewsutton97@outlook.com>
2023-04-28 11:48:02 +0100

    Add policy for accessing secrets to the lambda role. (#45)

--- aws-configuration/iam_role/LambdaExecution.tf
+++ aws-configuration/iam_role/LambdaExecution.tf
@@ -11,3 +11,24 @@ resource "aws_iam_role_policy_attachment" "lambda_execution_vpc_policy" {
 
   provider = aws.europe_london
 }
+
+resource "aws_iam_role_policy" "lambda_execution_secretsmanager_policy" {
+  name = "LambdaSecretsManagerExecutionRole"
+  role = aws_iam_role.lambda_execution.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "secretsmanager:DescribeSecret",
+          "secretsmanager:GetSecretValue"
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      } 
+    ]
+  })
+
+  provider = aws.europe_london
+}
