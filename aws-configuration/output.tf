1581bd5080b08ba0d50125e71cf98b00e21f3901
Matthew Sutton <matthewsutton97@outlook.com>
2023-04-28 08:03:09 +0100

    Add data iam that can be used from the app tier. (#40)
    * Add data iam that can be used from the app tier.
    
    * Format policy.

--- aws-configuration/output.tf
+++ aws-configuration/output.tf
@@ -6,3 +6,21 @@ output "iam_terraform_secret_access_key" {
   value     = module.iam.terraform_secret_access_key
   sensitive = true
 }
+
+output "iam_github_access_key" {
+  value = module.iam.github_access_key
+}
+
+output "iam_github_secret_access_key" {
+  value     = module.iam.github_secret_access_key
+  sensitive = true
+}
+
+output "iam_data_access_key" {
+  value = module.iam.data_access_key
+}
+
+output "iam_data_secret_access_key" {
+  value     = module.iam.data_secret_access_key
+  sensitive = true
+}
