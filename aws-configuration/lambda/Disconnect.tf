3124c2e4de4cb22f97f21450583e116f4bb9d340
Matthew Sutton <matthewsutton97@outlook.com>
2023-05-13 14:22:45 +0100

    Setting up log retention for lambdas and shortening timeout.

--- aws-configuration/lambda/Disconnect.tf
+++ aws-configuration/lambda/Disconnect.tf
@@ -8,7 +8,7 @@ resource "aws_lambda_function" "disconnect_function" {
 
   role             = data.aws_iam_role.lambda_iam_role.arn
 
-  timeout          = 30
+  timeout          = 5
   memory_size      = 128
 
   layers           = [ data.aws_lambda_layer_version.lambda_secretsmanager_layer.arn ]
@@ -20,6 +20,13 @@ resource "aws_lambda_function_event_invoke_config" "disconnect_function_config" 
   function_name                = aws_lambda_function.disconnect_function.function_name
   maximum_event_age_in_seconds = 60
   maximum_retry_attempts       = 1
+
+  provider = aws.europe_london
+}
+
+resource "aws_cloudwatch_log_group" "disconnect_function_logs" {
+  name              = "/aws/lambda/${aws_lambda_function.disconnect_function.function_name}"
+  retention_in_days = 14
 
   provider = aws.europe_london
 }
