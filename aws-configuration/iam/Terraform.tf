5b3863e4e7c49912b6c693968a2ac41ce76a5b5c
Matthew Sutton <matthewsutton97@outlook.com>
2023-05-13 14:18:54 +0100

    Give terraform user access to logs.

--- aws-configuration/iam/Terraform.tf
+++ aws-configuration/iam/Terraform.tf
@@ -213,3 +213,26 @@ EOF
 
   provider = aws.europe_london
 }
+
+resource "aws_iam_user_policy" "terraform_policy_cloudwatch" {
+  name     = "terraform-cloudwatch"
+  user     = aws_iam_user.terraform.name
+  policy   = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+          "logs:*"
+        ],
+      "Effect": "Allow",
+      "Resource": [
+          "*"
+        ]
+    }
+  ]
+}
+EOF
+
+  provider = aws.europe_london
+}
